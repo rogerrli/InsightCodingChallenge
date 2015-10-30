ffd59efa075dffcc89bd4479b6ed34a440943f64
Roger <li.rojie@gmail.com>
2015-10-30 19:05:55 -0400

    Unicode tweet counter
    Added that counter that checks how many tweets have unicode in it. This
    was especially difficult because Python 3 already designates str
    variables as unicode
    :unamused:

--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -7,14 +7,15 @@ def openFile():
     from sys import argv
     script, input_file, output_file = argv
     script_dir = os.path.dirname(__file__)
-    output_file = script_dir[0:len(script_dir) - 4] + output_file[1:]
-    input_file = script_dir[0:len(script_dir)-4] + input_file[1:]
-    outfile = open(output_file, 'w')
-    with open(input_file, 'r') as infile:
+    outfile = open(script_dir[0:len(script_dir) - 4] + output_file[1:], 'w')
+    unicode_tweets = 0
+    with open(script_dir[0:len(script_dir)-4] + input_file[1:], 'r') as infile:
         for line in infile:
             try:
                 jfile = json.loads(line)
                 try:
+                    if "\\u" in json.dumps(jfile['text']):
+                        unicode_tweets += 1
                     current_text = json.dumps(jfile['text']).encode('ascii').decode('unicode_escape')\
                         .encode('ascii', 'ignore').decode('utf8').replace("\n", " ").replace('"', '')
                     current_time = json.dumps(jfile['created_at']).encode('ascii').decode('unicode_escape')\
@@ -27,6 +28,8 @@ def openFile():
             except ValueError:
                 pass
     outfile.close()
+    print("Number of tweets with unicode in it: " + str(unicode_tweets))
+
 
 def main():
     openFile()
